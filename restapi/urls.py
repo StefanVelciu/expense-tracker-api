0867efbe0ebbbf46fa6106c5b69d343cfaf1e0a0
StefanVelciu <stefan.velciu@gmail.com>
2025-01-08 17:19:39 +0200

    Added creation and listing of expenses with APIView + test.

--- restapi/urls.py
+++ restapi/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for expense_tracker project.
+"""expense_tracker URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,10 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from restapi import views
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("api/", include(("restapi.urls", "restapi"), namespace="restapi")),
+    path("expenses/", views.ExpenseListCreate.as_view(), name="expense-list-create")
 ]
